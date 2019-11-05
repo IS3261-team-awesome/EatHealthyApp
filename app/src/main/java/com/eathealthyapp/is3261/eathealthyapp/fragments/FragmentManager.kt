9b7dea126d87192adab4ed31f8c5dbe5f441b6e5
Francis <francislow_6@hotmail.com>
2019-11-06 01:08:16 +0800

    Add indicator colour for selected tab

--- app/src/main/java/com/eathealthyapp/is3261/eathealthyapp/fragments/FragmentManager.kt
+++ app/src/main/java/com/eathealthyapp/is3261/eathealthyapp/fragments/FragmentManager.kt
@@ -5,40 +5,25 @@ import android.support.v4.view.ViewPager
 import android.view.ViewGroup
 import com.eathealthyapp.is3261.eathealthyapp.activities.MainActivity
 import com.eathealthyapp.is3261.eathealthyapp.R
+import com.eathealthyapp.is3261.eathealthyapp.activities.ActivityScanner
 
 class FragmentManager(val activity: MainActivity) {
     lateinit var viewPager: ViewPager
 
 
     fun setup() {
-        // Set viewpager
+        // Setup viewpager
         val fragmentPageAdaptor = FragmentPageAdaptor(activity.supportFragmentManager)
         viewPager = activity.findViewById<ViewPager>(R.id.myvp)
         viewPager.adapter = fragmentPageAdaptor
         viewPager.offscreenPageLimit = 3
         viewPager.currentItem = 0
 
-        // To make camera and detection start only when it is on camera fragment
-       /* viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageSelected(position: Int) {
-                val scannerFragment: ActivityScanner = fragmentPageAdaptor.getItem(1) as ActivityScanner
-                if (position == 1) {
-                    scannerFragment.startDetection()
-                } else {
-                    scannerFragment.stopDetection()
-                }
-            }
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-            override fun onPageScrollStateChanged(state: Int) {}
-        })*/
-
         // Setup Btm Tab
         val btmTabLayout = activity.findViewById<TabLayout>(R.id.btmViewPagerTabLayout)
         btmTabLayout.setupWithViewPager(viewPager)
-        btmTabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_home)
-        //btmTabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_camera)
+        btmTabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_home_color)
         btmTabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_wallet)
-
         btmTabLayout.setSelectedTabIndicatorHeight(0)
 
         val tab0 = (btmTabLayout.getChildAt(0) as ViewGroup).getChildAt(0)
@@ -48,6 +33,21 @@ class FragmentManager(val activity: MainActivity) {
         val tab1 = (btmTabLayout.getChildAt(0) as ViewGroup).getChildAt(1)
         val p2 = tab1.getLayoutParams() as ViewGroup.MarginLayoutParams
         p2.setMargins(200, 0, 0, 0)
+
+        // Add listener to change colour of icon when fragment is selected
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                if (position == 0) {
+                    btmTabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_home_color)
+                    btmTabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_wallet)
+                } else if (position == 1) {
+                    btmTabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_home)
+                    btmTabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_wallet_color)
+                }
+            }
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
     }
 
     fun setSelectedFragment(fragmentNumber: Int) {
